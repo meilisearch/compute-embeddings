99901141eeccc45d186b90aff92d81ec67a2fb9f
Kerollmops <clement@meilisearch.com>
2023-06-07 11:56:39 +0200

    Chunk the documents and display an ETA

--- src/bin/dataset.rs
+++ src/bin/dataset.rs
@@ -1,8 +1,10 @@
 use std::io::{self, BufReader, BufWriter};
+use std::time::Instant;
 
-use indicatif::ProgressIterator;
+use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
+use itertools::Itertools;
 use rust_bert::pipelines::sentence_embeddings::{
-    SentenceEmbeddingsBuilder, SentenceEmbeddingsModel, SentenceEmbeddingsModelType,
+    SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
 };
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
@@ -28,6 +30,23 @@ struct Input {
     object_id: String,
 }
 
+impl Input {
+    fn text(&self) -> String {
+        let Input {
+            name,
+            description,
+            brand,
+            categories,
+            _type,
+            object_id,
+            ..
+        } = self;
+
+        let categories = categories.join(" ");
+        format!("{name} {description} {brand} {categories} {_type} {object_id}")
+    }
+}
+
 // {
 //   "points": [
 //     {"id": 1, "vector": [0.05, 0.61, 0.76, 0.74], "payload": {"city": "Berlin" }}
@@ -49,18 +68,34 @@ fn main() -> anyhow::Result<()> {
     let reader = BufReader::new(io::stdin());
     let documents: Vec<Input> = serde_json::from_reader(reader)?;
 
+    let progress_style = ProgressStyle::with_template("{wide_bar} {pos}/{len} {eta}").unwrap();
+    let progress_bar = ProgressBar::new(documents.len() as u64).with_style(progress_style);
+
     // Set-up sentence embeddings model
+    let now = Instant::now();
     let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL6V2)
         .create_model()?;
 
+    eprintln!("It took {:.02?} to initialize the model.", now.elapsed());
+
     let mut points = Vec::new();
-    for (id, payload) in documents.into_iter().enumerate().progress() {
-        let vector = compute_embedding(&model, &payload)?;
-        points.push(Point {
-            id,
-            vector,
-            payload,
-        });
+    for chunk in documents
+        .into_iter()
+        .enumerate()
+        .progress_with(progress_bar)
+        .chunks(4)
+        .into_iter()
+    {
+        let chunk: Vec<_> = chunk.collect();
+        let sentences: Vec<_> = chunk.iter().map(|(_, payload)| payload.text()).collect();
+        let vectors = model.encode(&sentences)?;
+        for ((id, payload), vector) in chunk.into_iter().zip(vectors) {
+            points.push(Point {
+                id,
+                vector,
+                payload,
+            });
+        }
     }
 
     let output = Output { points };
@@ -68,23 +103,4 @@ fn main() -> anyhow::Result<()> {
     serde_json::to_writer_pretty(writer, &output)?;
 
     Ok(())
-}
-
-fn compute_embedding(
-    model: &SentenceEmbeddingsModel,
-    document: &Input,
-) -> anyhow::Result<Vec<f32>> {
-    let Input {
-        name,
-        description,
-        brand,
-        categories,
-        _type,
-        object_id,
-        ..
-    } = document;
-
-    let categories = categories.join(" ");
-    let sentence = format!("{name} {description} {brand} {categories} {_type}, {object_id}");
-    Ok(model.encode(&[&sentence])?.remove(0))
 }
--- src/bin/query.rs
+++ src/bin/query.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use std::{env, io};
 
 use rust_bert::pipelines::sentence_embeddings::{
@@ -5,15 +6,17 @@ use rust_bert::pipelines::sentence_embeddings::{
 };
 
 fn main() -> anyhow::Result<()> {
-    if let Some(query) = env::args().nth(1) {
-        // Set-up sentence embeddings model
-        let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL6V2)
-            .create_model()?;
+    let query = env::args().nth(1).unwrap_or_default();
+    let now = Instant::now();
+    // Set-up sentence embeddings model
+    let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL6V2)
+        .create_model()?;
 
-        let vector = model.encode(&[&query])?.remove(0);
-        let writer = io::stdout();
-        serde_json::to_writer(writer, &vector)?;
-    }
+    eprintln!("It took {:.02?} to initialize the model.", now.elapsed());
+
+    let vector = model.encode(&[&query])?.remove(0);
+    let writer = io::stdout();
+    serde_json::to_writer(writer, &vector)?;
 
     Ok(())
 }
