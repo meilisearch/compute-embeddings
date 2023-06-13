6242c77f4f80c81a673da309eaa17c0df8d3a39c
Kerollmops <clement@meilisearch.com>
2023-06-13 18:10:05 +0200

    Make it work with OpenAI, missing Query binary

--- src/bin/ce-dataset.rs
+++ src/bin/ce-dataset.rs
@@ -1,7 +1,10 @@
 use clap::{Parser, ValueEnum};
+use std::env::var;
 use std::io::{self, BufReader, BufWriter};
-use std::time::Instant;
+use std::thread;
+use std::time::{Duration, Instant};
 
+use anyhow::bail;
 use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 use itertools::Itertools;
 use rust_bert::pipelines::sentence_embeddings::{
@@ -21,14 +24,25 @@ struct Args {
     #[arg(long, default_value_t = 4)]
     batched_documents: usize,
 
+    /// The API you want to use to compute the embeddings.
+    #[arg(long)]
+    semantic_api: SemanticApi,
+
     /// The fields to concatenate in this specific order to generate the embeddings.
     documents_fields: Vec<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum SemanticApi {
+    OpenAi,
+    AllMiniLmL6V2,
 }
 
 fn main() -> anyhow::Result<()> {
     let Args {
         documents_style,
         batched_documents,
+        semantic_api,
         documents_fields,
     } = Args::parse();
 
@@ -58,7 +72,15 @@ fn main() -> anyhow::Result<()> {
             .iter()
             .map(|(_, payload)| payload.text(&documents_fields))
             .collect();
-        let vectors = model.encode(&sentences)?;
+
+        let vectors = match semantic_api {
+            SemanticApi::OpenAi => {
+                let api_key = var("OPENAI_API_KEY").expect("missing OPENAI_API_KEY env variable");
+                openai_vectors(sentences, &api_key)?
+            }
+            SemanticApi::AllMiniLmL6V2 => model.encode(&sentences)?,
+        };
+
         for entry in chunk.into_iter().zip(vectors) {
             output.push(entry);
         }
@@ -177,4 +199,68 @@ struct Point {
 enum DocumentStyle {
     Meilisearch,
     Qdrant,
+}
+
+#[derive(Debug, Deserialize)]
+struct OpenAiResponse {
+    data: Vec<Embedding>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Embedding {
+    embedding: Vec<f32>,
+    // object: String,
+    // index: usize,
+}
+
+fn openai_vectors(sentences: Vec<String>, openai_api_key: &str) -> anyhow::Result<Vec<Vec<f32>>> {
+    use ureq::Error;
+
+    let mut sentences = sentences;
+    let mut wait_for = Duration::from_secs(2);
+    for _ in 0..100 {
+        let result = ureq::post("https://api.openai.com/v1/embeddings")
+            .set("Authorization", &format!("Bearer {openai_api_key}"))
+            .send_json(ureq::json!({
+                "model": "text-embedding-ada-002".to_string(),
+                "input": sentences,
+            }));
+        match result {
+            Err(Error::Status(503 | 429, response)) => {
+                let response = response.into_string()?;
+                eprintln!("Retrying after {:.02?}: {}", wait_for, response);
+                thread::sleep(wait_for);
+                wait_for *= 2;
+            }
+            Err(Error::Status(400, response)) => {
+                // Most of the time it is due to the OpenAI 8191 max tokens
+                let max_length = sentences.iter().map(|s| s.len()).max().unwrap();
+                let cut_at = max_length * 80 / 100;
+                eprintln!(
+                    "Seeing error cutting sentences from max {max_length} to {cut_at}: {}",
+                    response.into_string()?,
+                );
+                sentences = sentences
+                    .into_iter()
+                    .map(|mut s| {
+                        s.truncate(cut_at);
+                        s
+                    })
+                    .collect();
+            }
+            Err(Error::Status(_, resp)) => {
+                bail!(
+                    "Cannot query OpenAI due to a {} status code. {}",
+                    resp.status(),
+                    resp.into_string()?,
+                )
+            }
+            Err(transport) => bail!("Cannot query OpenAI due to {}", transport),
+            Ok(response) => {
+                let response: OpenAiResponse = response.into_json()?;
+                return Ok(response.data.into_iter().map(|d| d.embedding).collect());
+            }
+        }
+    }
+    bail!("Cannot query OpenAI, too many retry")
 }
